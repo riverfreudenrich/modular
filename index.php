d21f5f6905a7bb53d99dc03a435897dc4622a5a4
River Freudenrich <riverfreudenrich@gmail.com>
2019-05-29 14:01:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 <div id="modular">
 <?php while ( have_rows('modular') ) : the_row(); ?>
 
-		<?php if (get_row_layout() == 'slider') : ?>
-			<!-- Add your slider/slider fields here -->
-		<?php endif; ?>
+    <?php if (get_row_layout() == 'slider') : ?>
+    	<!-- Add your slider/slider fields here -->
+    <?php endif; ?>
 
-		<?php if (get_row_layout() == 'content') : ?>
-			<!-- Add your content/content fields here -->
-		<?php endif; ?>
+    <?php if (get_row_layout() == 'content') : ?>
+    	<!-- Add your content/content fields here -->
+    <?php endif; ?>
 
-		<?php if (get_row_layout() == 'flexbox') : ?>
-			<!-- Add your flexbox/flexbox fields here -->
-		<?php endif; ?>
+    <?php if (get_row_layout() == 'flexbox') : ?>
+    	<!-- Add your flexbox/flexbox fields here -->
+    <?php endif; ?>
 
-		<?php if (get_row_layout() == 'icons') : ?>
-			<!-- Add your icons/icon fields here -->
-		<?php endif; ?>
+    <?php if (get_row_layout() == 'icons') : ?>
+    	<!-- Add your icons/icon fields here -->
+    <?php endif; ?>
 
 <?php endwhile; ?>
 </div>
