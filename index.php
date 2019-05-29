18f1ebe0560e1d5c88783c6cb99f2b5a671dae1b
River Freudenrich <riverfreudenrich@gmail.com>
2019-05-29 13:24:33 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php if( have_rows('modular') ): ?>
+<div id="modular">
+<?php while ( have_rows('modular') ) : the_row(); ?>
+
+		<?php if (get_row_layout() == 'slider') : ?>
+			<!-- Add your slider/slider fields here -->
+		<?php endif; ?>
+
+		<?php if (get_row_layout() == 'content') : ?>
+			<!-- Add your content/content fields here -->
+		<?php endif; ?>
+
+		<?php if (get_row_layout() == 'flexbox') : ?>
+			<!-- Add your flexbox/flexbox fields here -->
+		<?php endif; ?>
+
+		<?php if (get_row_layout() == 'icons') : ?>
+			<!-- Add your icons/icon fields here -->
+		<?php endif; ?>
+
+<?php endwhile; ?>
+</div>
+<?php endif; ?>
